86af6239cee8d5da77f5877501320d3237b02854
IYake <iangyake@gmail.com>
2017-02-18 12:27:47 -0600

    Update twitterBot.py
    In the middle of adding wolfram alpha operations

--- twitterBot.py
+++ twitterBot.py
@@ -2,25 +2,15 @@
 Created on Fri Oct 25 21:51:08 2017
 @author: Devin Suttles
 """
-import account
-
+#import wolframalpha
 import tweepy     
-
-def isWordInString(word, string, matches, index):
-    if matches == (len(word)):
-        return True
-    elif index > (len(string) - 1):
-        return False
-    elif word[matches] == string[index]:
-        return isWordInString(word, string, matches + 1, index + 1)
-    elif word[matches] != string[index]:
-        return isWordInString(word, string, 0, index + 1)
 
 #Use your keys
 consumer_key = "LwI9JtZEFE8JpxDAdTuKJj7cc"#account.key
 consumer_secret = "SHm5nXAb0iMZHJF4qPNwSx2WFalLybOFPfNc2ReJ1CkeuidLfD"#account.secret 
 access_token = "480036925-UviFhAhHmBz6W0xtn4p5KxSeT5O8ogDKuqUjir0K"#account.accessToken
 access_secret = "uxmFiXzeUitAMq0NDDNrpYAwI3rA2z42jyOCHtJOVUJiL"#account.accessTokenSecret
+wolf_ID="KQP6LU-6GU23P4A6L"
 
 
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
@@ -38,12 +28,16 @@ for tweet in public_tweets:
 
 
 """
+#client = wolframalpha.Client(wolf_ID)
+
+#res = client.query("iubawgiub")
+
 #override tweepy.StreamListener to add logic to on_status
 class MyStreamListener(tweepy.StreamListener):
 
     def on_status(self, status):
-        print(status.text)
-        api.update_status(".@" + status.user.screen_name + " Time", status.id)
+        print(status.text[14:])
+        api.update_status(".@" + status.user.screen_name + " asdf", status.id)
         
     def on_error(self, status_code):
         if status_code == 420:
