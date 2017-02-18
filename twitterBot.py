ecaf785b63b92d07bc5ecdd8bfb1317d38c0374f
Devin Suttles <devinsuttles@outlook.com>
2017-02-18 08:34:06 -0600

    Update twitterBot.py

--- twitterBot.py
+++ twitterBot.py
@@ -10,4 +10,8 @@ consumer_secret = "secret"
 access_token = "access token"
 access_secret= "access token secret" 
 
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_token_secret)
+
+api = tweepy.API(auth)
 
