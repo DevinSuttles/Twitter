09f34a7bd3df778338b2fe1e51a82960b8fba6da
IYake <iangyake@gmail.com>
2017-02-18 09:07:34 -0600

    Update twitterBot.py

--- twitterBot.py
+++ twitterBot.py
@@ -1,17 +1,53 @@
 """
 Created on Fri Oct 25 21:51:08 2017
 @author: Devin Suttles
-
 """
-import tweepy 
+import account
 
-consumer_key = "key" 
-consumer_secret = "secret" 
-access_token = "access token"
-access_secret= "access token secret" 
+import tweepy     
+
+def isWordInString(word, string, matches, index):
+    if matches == (len(word)):
+        return True
+    elif index > (len(string) - 1):
+        return False
+    elif word[matches] == string[index]:
+        return isWordInString(word, string, matches + 1, index + 1)
+    elif word[matches] != string[index]:
+        return isWordInString(word, string, 0, index + 1)
+
+#Use your keys
+consumer_key = "LwI9JtZEFE8JpxDAdTuKJj7cc"#account.key
+consumer_secret = "SHm5nXAb0iMZHJF4qPNwSx2WFalLybOFPfNc2ReJ1CkeuidLfD"#account.secret 
+access_token = "480036925-UviFhAhHmBz6W0xtn4p5KxSeT5O8ogDKuqUjir0K"#account.accessToken
+access_secret = "uxmFiXzeUitAMq0NDDNrpYAwI3rA2z42jyOCHtJOVUJiL"#account.accessTokenSecret
+
 
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
-auth.set_access_token(access_token, access_token_secret)
+auth.set_access_token(access_token, access_secret)
 
 api = tweepy.API(auth)
 
+public_tweets = api.home_timeline(count = 10)
+for tweet in public_tweets:
+    print tweet.text
+    if isWordInString("@DevinSuttles", tweet.text, 0, 0):
+        api.update_status(api.update_status(".@" + tweet.user.screen_name + " Time", tweet.id))#change @Ian Yake to user of tweet
+    #print tweet.text
+
+
+"""
+#override tweepy.StreamListener to add logic to on_status
+class MyStreamListener(tweepy.StreamListener):
+
+    def on_status(self, status):
+        print(status.text)
+        
+    def on_error(self, status_code):
+        if status_code == 420:
+            #returning False in on_data disconnects the stream
+            return False
+        
+myStreamListener = MyStreamListener()
+myStream = tweepy.Stream(auth = api.auth, listener=MyStreamListener())
+"""
