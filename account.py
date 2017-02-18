e5293f36d2a618e60313280a21ef1f4e9e455271
Devin Suttles <devinsuttles@outlook.com>
2017-02-18 00:40:45 -0600

    Add files via upload

---
+++ account.py
@@ -0,0 +1,13 @@
+""" 
+
+Created on Fri Feb 17 22:10:40 2017
+@author: Devin Suttles
+
+"""  
+file = open("Twitter.txt","r")
+key = file.readline()
+secret = file.readline()
+owner = file.readline() 
+ID = file.readline()
+accessToken = file.readline()
+accessTokenSecret= file.readline()
