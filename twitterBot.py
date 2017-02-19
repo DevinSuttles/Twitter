f53e2a384a1ac190f09bb7597b74e602cc564455
IYake <iangyake@gmail.com>
2017-02-18 18:33:39 -0600

    Update twitterBot.py
    Handles exceptions/errors

--- twitterBot.py
+++ twitterBot.py
@@ -5,12 +5,12 @@ Created on Fri Oct 25 21:51:08 2017
 import wolframalpha
 import tweepy     
 
-#Use your keys
-consumer_key = ""#account.key
-consumer_secret = ""#account.secret 
-access_token = ""#account.accessToken
-access_secret = ""#account.accessTokenSecret
-wolf_ID="" #wolfram alpha app id
+#Use your keys, from Twitter
+consumer_key = ""
+consumer_secret = ""
+access_token = ""
+access_secret = ""
+wolf_ID=""#Wolfram Alpha App ID
 
 
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
@@ -25,10 +25,23 @@ class MyStreamListener(tweepy.StreamListener):
 
     def on_status(self, status):
         print(status.text[11:])
-        res = client.query(status.text[11:])
-        first = next(res.results, None)
-        if first:
-            api.update_status(".@" + status.user.screen_name + " " + first.text, status.id)
+        if status.text[11:] != "":#In case twitter user gives empty tweet
+            res = client.query(status.text[11:])
+            try:
+                try:
+                    first = next(res.results, None)
+                except AttributeError:
+                    print "Attribute Error from " +status.user.screen_name
+                    pass
+                if first:
+                    try:
+                        api.update_status(".@" + status.user.screen_name + " " + first.text, status.id)
+                    except Exception:
+                        print "Exception Error from " + status.user.screen_name
+                        pass
+            except UnboundLocalError:
+                print "UnboundLocalError from " + status.user.screen_name
+                pass
         
     def on_error(self, status_code):
         if status_code == 420:
